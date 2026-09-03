c7d49aa8445011beadc1cc5498271b99f0acbf31
Sritha Chowdary K <srithakchowdary@gmail.com>
2026-09-03 23:52:36 +0530

    user.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,13 +1,10 @@
 const mongoose = require("mongoose");
+
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true, trim: true },
   password: { type: String, required: true },
-<<<<<<< HEAD
   role: { type: String, enum: ["Admin", "User"], default: "User" },
   createdAt: { type: Date, default: Date.now },
 });
-=======
-  createdAt: { type: Date, default: Date.now },
-});
+
 module.exports = mongoose.model("User", userSchema);
->>>>>>> bbbd655cfb96b3874fbe09c0375c01389f20f29a
